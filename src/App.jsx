f6781c16e00e2869a40c14004f7ea9b60a9af0ac
Jecheon Ryu <rjc1704@gmail.com>
2024-06-07 11:06:15 +0900

    calcuator redux 문제

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
+      <div>
+        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
+      </div>
+      <hr />
+      <div>
+        <h3>결과</h3>
+        <p>0</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
